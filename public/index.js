2aa3b514d08bf794703326fcba404279be4b0971
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-03-20 20:53:49 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -30,7 +30,9 @@ function openURL(url) {
       if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
       else if (!(url.startsWith("https://") || url.startsWith("http://")))
           url = "http://" + url;
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+          localStorage.setItem('iframeurl', __uv$config.prefix + __uv$config.encodeUrl(url))
+          window.location.href = "./iframe.html"
+  
     });
 };
 
